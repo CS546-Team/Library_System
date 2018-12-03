018e60960ea70fc16a31a466b231fe661377f951
Master-Alcy <aijingxuan92@gmail.com>
2018-12-03 14:10:44 -0500

    auth solved

--- Code/client/src/Components/App.jsx
+++ Code/client/src/Components/App.jsx
@@ -52,10 +52,12 @@ const Staffs = ({ match }) => (
 class App extends Component {
     constructor() {
         super();
-        this.protector = this.protector.bind(this);
+        this.state = {
+            loader: 'init'
+        }
     }
 
-    protector() {
+    componentDidMount() {
         debugger;
         const student = cookies.get('student');
         const staff = cookies.get('staff');
@@ -74,7 +76,7 @@ class App extends Component {
                 headers: {
                     'Content-type': 'application/json'
                 },
-                body: JSON.stringify({isFind: false})
+                body: JSON.stringify({ isFind: false })
             })
                 .then((response) => response.json())
                 .then((result) => {
@@ -84,19 +86,60 @@ class App extends Component {
                         debugger;
                         if (isStudent) {
                             debugger;
-                            return (<Route name="student" path="/student" component={Students} />);
+                            this.setState({
+                                loader: "student"
+                            });
+                            return;
                         }
                         debugger;
-                        return (<Route name="staff" path="/staff" component={Staffs} />);
+                        this.setState({
+                            loader: "staff"
+                        });
                     }
                 });
         } catch (err) {
-            return (<Route component={NotFound} />);
+            this.setState({
+                loader: "notfound"
+            });
         }
     }
 
-
     render() {
+        const input = this.state.loader;
+        console.log(input);
+        if (input === 'student') {
+            return (
+                <Router>
+                    <div>
+                        <NavBar />
+                        <Switch>
+                            <Route name="home" exact path="/" component={HomePage} />
+                            <Route name="sign" exact path="/signup" component={SignupPage} />
+                            <Route name="log" exact path="/login" component={LoginPage} />
+                            <Route name="student" path="/student" component={Students} />
+                            <Route component={NotFound} />
+                        </Switch>
+                        <Footer />
+                    </div>
+                </Router>
+            );
+        } else if (input === 'staff') {
+            return (
+                <Router>
+                    <div>
+                        <NavBar />
+                        <Switch>
+                            <Route name="home" exact path="/" component={HomePage} />
+                            <Route name="sign" exact path="/signup" component={SignupPage} />
+                            <Route name="log" exact path="/login" component={LoginPage} />
+                            <Route name="staff" path="/staff" component={Staffs} />
+                            <Route component={NotFound} />
+                        </Switch>
+                        <Footer />
+                    </div>
+                </Router>
+            );
+        }
         return (
             <Router>
                 <div>
@@ -105,12 +148,12 @@ class App extends Component {
                         <Route name="home" exact path="/" component={HomePage} />
                         <Route name="sign" exact path="/signup" component={SignupPage} />
                         <Route name="log" exact path="/login" component={LoginPage} />
-                        {this.protector()}
+                        <Route component={NotFound} />
                     </Switch>
                     <Footer />
                 </div>
             </Router>
-        )
+        );
     }
 }
 
